701e563be3fac74c9bfe3c05d80a67144250d500
Gabriel H. Dalmoro <ghdalmoro@gmail.com>
2022-10-07 15:21:52 -0600

    fixed edge detection so player stays within map; added map graphics for every quadrant to show player location; installed modules for font customization.

--- draft-code.js
+++ draft-code.js
@@ -1,33 +1,67 @@
 //basic structure to the game
-import { theMap } from './graphics.js';
+import * as graphics from './graphics.js';
+import cfonts from 'cfonts';
 import rl from 'readline-sync';
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+
 const log = console.log;
 function locationLog(x, y) {
   log(`Your location is: x = ${x}, y = ${y}.`);
 }
-//MAKE GIT COMMITS!!!!!!!!!!!!
+function directionLogic(direction) {
+  let newX = x;
+  let newY = y;
+  direction === 'north' || direction === 'up'
+    ? newY++
+    : direction === 'south' || direction === 'down'
+    ? newY--
+    : direction === 'east' || direction === 'right'
+    ? newX++
+    : direction === 'west' || direction === 'left'
+    ? newX--
+    : log(chalk.yellow('command not valid'));
+  if (newX <= 3 && newX >= 1 && newY <= 3 && newY >= 1) {
+    x = newX;
+    y = newY;
+    locationLog(x, y);
+  } else {
+    log(chalk.red.bold('You cant go that way.'));
+  }
+}
 
+function roomDescriptions(x, y) {
+  x === 1 && y === 1
+    ? log(graphics.map11)
+    : x === 1 && y === 2
+    ? log(graphics.map12)
+    : x === 1 && y === 3
+    ? log(graphics.map13)
+    : x === 2 && y === 1
+    ? log(graphics.map21)
+    : x === 2 && y === 2
+    ? log(graphics.map22)
+    : x === 2 && y === 3
+    ? log(graphics.map23)
+    : x === 3 && y === 1
+    ? log(graphics.map31)
+    : x === 3 && y === 2
+    ? log(graphics.map32)
+    : log(graphics.map33);
+}
+
+cfonts.say("Gabriel's amazing game", {
+  gradient: ['yellow', 'green'],
+  font: 'block',
+  background: 'transparent',
+});
 log('You are in the start');
-locationLog(2, 2);
-log(theMap);
+log(graphics.map22);
 let x = 2;
 let y = 2;
-while (1 >= x <= 3 && 1 >= y <= 3) {
+
+while (true) {
   let chooseDirection = rl.question('Where would you like to go? ');
-  //   if (x <= 3 && x >= 1 && y <= 3 && y >= 1) {
-  function directionLogic(direction) {
-    direction === 'north'
-      ? y++ && locationLog(x, y)
-      : direction === 'south'
-      ? y-- && locationLog(x, y)
-      : direction === 'east'
-      ? x++ && locationLog(x, y)
-      : direction === 'west'
-      ? x-- && locationLog(x, y)
-      : log('command not valid');
-  }
   directionLogic(chooseDirection);
-  //   } else {
-  //     log("You can't go that way.");
-  //   }
+  roomDescriptions(x, y);
 }
---
+++ test.js
@@ -0,0 +1,13 @@
+import {
+  map11,
+  map12,
+  map13,
+  map21,
+  map22,
+  map23,
+  map31,
+  map32,
+  map33,
+} from './graphics.js';
+
+console.log(map22);
